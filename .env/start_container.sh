ff38e23f93ea0fc513a5a5e58d3f492c97e720b5
Yasen Pavlov <91540875+yasen42@users.noreply.github.com>
2021-10-10 12:20:46 +0200

    add git ignores

---
+++ .env/start_container.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#set variables
+UID=1000
+GID=1000
+UNAME=jupyter
+CWD=$(dirname $(realpath "$0"))
+WORKBENCH_DIR=/home/$USER/Workbench
+
+#set cwd
+cd $CWD
+mkdir -p $CWD/home
+
+#build image
+docker build -f Dockerfile.dev  \
+    -t muty/co3320-ngc-jupyter \
+    --build-arg GID=$GID \
+    --build-arg UID=$UID \
+    --build-arg UNAME=$UNAME \
+    --build-arg GNAME=$UNAME .
+
+#run container
+docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --gpus all -it --rm \
+    --name co3320-ngc-jupyter \
+    --user 1000:1000 \
+    -v $WORKBENCH_DIR:/workspace/workbench \
+    -v $CWD/home:/home/$UNAME \
+    -p 8888:8888 \
+    muty/co3320-ngc-jupyter
